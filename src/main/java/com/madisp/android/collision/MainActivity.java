136b20b93bbbee5bc2faa42ca79eb8153177afd8
Madis Pink <madis.pink@gmail.com>
2013-12-29 23:00:42 +0200

    Initial code dump

---
+++ src/main/java/com/madisp/android/collision/MainActivity.java
@@ -0,0 +1,20 @@
+package com.madisp.android.collision;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+		ImageView iv = new ImageView(this);
+
+		iv.setBackgroundResource(R.color.transparent);
+		iv.setImageResource(R.drawable.ic_launcher);
+
+		final int matchParent = ViewGroup.LayoutParams.MATCH_PARENT;
+		setContentView(iv, new ViewGroup.LayoutParams(matchParent, matchParent));
+    }
+}
