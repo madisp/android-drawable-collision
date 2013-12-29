a8ab3b269e80b386c196aef4bb6004de48a41f7e
Madis Pink <madis.pink@gmail.com>
2013-12-30 00:29:20 +0200

    Fix whitespace

--- src/main/java/com/madisp/android/collision/MainActivity.java
+++ src/main/java/com/madisp/android/collision/MainActivity.java
@@ -6,13 +6,13 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		ImageView iv = new ImageView(this);
 		iv.setBackgroundResource(R.color.transparent);
 		iv.setImageResource(R.drawable.ic_launcher);
 		final int matchParent = ViewGroup.LayoutParams.MATCH_PARENT;
 		setContentView(iv, new ViewGroup.LayoutParams(matchParent, matchParent));
-    }
+	}
 }
